cba3638930e22f60053ab516ac47cd1f85d9df69
Rumasoreng Fadil <limitvariabel@gmail.com>
2025-05-27 13:09:04 +0700

    chore (feature) : Created ShowcaseList

---
+++ src/design-system/molecules/ShowcaseList.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import SkillBadgeList from "../components/SkillBadgeList";
+
+export type ShowcaseItem = {
+  title: string;
+  description: string;
+  skills: string[];
+  link: string;
+  image: string;
+};
+
+type ShowcaseListProps = {
+  items: ShowcaseItem[];
+};
+
+const ShowcaseList = ({ items }: ShowcaseListProps) => {
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
+  return (
+    <div className="flex flex-col space-y-12">
+      {items.map(({ title, description, skills, link, image }, i) => (
+        <Link
+          target="_blank"
+          href={link}
+          key={i}
+          onMouseEnter={() => setHoveredIndex(i)}
+          onMouseLeave={() => setHoveredIndex(null)}
+          className={`group flex flex-col-reverse gap-4 transition-all sm:p-4 rounded-md cursor-pointer sm:flex-row sm:gap-0 sm:space-x-10
+            ${hoveredIndex !== i && hoveredIndex !== null ? "lg:opacity-40" : ""}
+            ${hoveredIndex === i ? "lg:bg-slate-400 lg:shadow-lg" : ""}
+          `}
+        >
+          <div className="relative w-36 h-20">
+            <Image
+              className="rounded-lg border-2 border-slate-500"
+              src={`/${image}`}
+              alt={title}
+              fill
+              priority
+            />
+          </div>
+
+          <div className="flex flex-col flex-1 space-y-3">
+            <div className="flex items-center space-x-1 text-lg font-semibold">
+              <h1>{title}</h1>
+              <span className={`transition-all ${hoveredIndex === i ? "translate-x-2" : ""}`}>{"->"}</span>
+            </div>
+            <p>{description}</p>
+            <SkillBadgeList skills={skills} />
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default ShowcaseList;
