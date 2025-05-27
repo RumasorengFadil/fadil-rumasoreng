81d4d8be10bd36603f8918023bb6b5fbe015399e
Rumasoreng Fadil <limitvariabel@gmail.com>
2025-05-27 11:30:48 +0700

    chore () : Redeploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import CustomCursor from "@/design-system/components/CustomCursor";
 
 
 const geistSans = localFont({
